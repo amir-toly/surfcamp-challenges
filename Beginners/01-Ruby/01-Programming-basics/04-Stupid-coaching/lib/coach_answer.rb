9604df54624e9a8b6fde7f98b95b447c12e525d7
Amir TOLY <amir.toly@gmail.com>
2014-09-08 23:43:57 +0200

    04-Stupid-coaching

--- Beginners/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/coach_answer.rb
+++ Beginners/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,22 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message == 'I am going to work right now SIR !'
+    ""
+  elsif your_message.strip.reverse[0] == '?'
+    "Silly question, get dressed and go to work !"
+  else
+    "I don't care son, get dressed and go to work !"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours !
+  supportive_msg = ''
+
+  if your_message == your_message.upcase
+    supportive_msg = "I can feel your motivation son ! But..." + "\n"
+  end
+
+  supportive_msg + coach_answer(your_message)
 end
 
--- Beginners/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/interface.rb
+++ Beginners/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,9 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
 
+answer_from_coach = 'What do you have to say son?'
+
+while answer_from_coach != ''
+  puts answer_from_coach
+  answer_from_coach = coach_answer_enhanced(gets.chomp)
+end
