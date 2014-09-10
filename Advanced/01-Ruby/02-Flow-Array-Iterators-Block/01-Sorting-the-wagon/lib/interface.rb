7b001a867508733578570768ff5eec3ad8f96f55
Amir TOLY <amir.toly@gmail.com>
2014-09-10 10:10:11 +0200

    01-Sorting-the-wagon

--- Advanced/01-Ruby/02-Flow-Array-Iterators-Block/01-Sorting-the-wagon/lib/interface.rb
+++ Advanced/01-Ruby/02-Flow-Array-Iterators-Block/01-Sorting-the-wagon/lib/interface.rb
@@ -8,7 +8,16 @@ begin
   name.chomp! if name
 
   # TODO: Add the name we just gotto the students array
+  students << name
 
 end while name != ""
 
+# Remove the last (empty) element
+students.delete_at(students.length - 1)
+
 # TODO: Call `wagon_sort` method and display the sorted student list
+sorted_students = wagon_sort(students)
+
+puts "Congratulations! Your Wagon has #{sorted_students.length} students:" + "\n" +
+"- #{sorted_students[0...sorted_students.length - 1].join(', ')} " +
+"and #{sorted_students[sorted_students.length - 1]}"
