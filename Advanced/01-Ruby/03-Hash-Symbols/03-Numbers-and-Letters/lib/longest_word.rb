8bbf707a315324e510163581a4893ef19c66cec3
Amir TOLY <amir.toly@gmail.com>
2014-09-10 16:58:10 +0200

    WIP

--- Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
+++ Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
@@ -3,8 +3,15 @@ require 'json'
 
 def generate_grid(grid_size)
   #TODO: generate random grid of letters
+  ('A'..'Z').to_a.shuffle[0...grid_size]
 end
 
 def run_game(attempt, grid, start_time, end_time)
   #TODO: runs the game and return detailed hash of result
+  {
+    time: (end_time - start_time),
+    translation: "Look at wordreference.com",
+    score: "any idea?",
+    message: "well done"
+  }
 end
