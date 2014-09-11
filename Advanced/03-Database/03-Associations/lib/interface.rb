a7d80e0c912a8930b8436956e0defecb0984ba32
Amir TOLY <amir.toly@gmail.com>
2014-09-11 23:11:32 +0200

    03-Associations

--- Advanced/03-Database/03-Associations/lib/interface.rb
+++ Advanced/03-Database/03-Associations/lib/interface.rb
@@ -16,14 +16,28 @@ def create_post(user)
   post_params = { name: name, source_url: source_url, date: Time.now, rating: rating }
 
   # TODO: use ActiveRecord to add a new post for the user logged in!
+  post_params[:user_id] = user.id
+  Post.create(post_params)
 end
 
 def list_posts(user)
   # TODO: use ActiveRecord to get all posts of the current user
+  posts = Post.where(user_id: user.id)
+
+  puts '-' * 50
+  puts '%-3s %-15s %-20s %-30s %s' % ['#', 'Name', 'Source URL', 'Created At', 'Rating']
+
+  posts.each do |post|
+    puts '%-3d %-15s %-20s %-30s %d' % [post.id, post.name, post.source_url, post.date, post.rating]
+  end
+
+  puts '-' * 50
 end
 
 def delete_posts(user)
   # TODO: use ActiveRecord to delete all posts of current user
+  posts = Post.where(user_id: user.id)
+  posts.destroy_all
 end
 
 while true
@@ -32,6 +46,9 @@ while true
   until logged_in
     puts 'Please login with your <id>'
     # TODO: instantiate a user with his <id>
+    user = User.first
+
+    logged_in = true
   end
 
   puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
@@ -40,7 +57,7 @@ while true
   puts "3. Delete all posts"
   puts "4. Exit"
 
-  case ask('>')
+  case ask('>').to_i
   when 1 then create_post(user)
   when 2 then list_posts(user)
   when 3 then delete_posts(user)
