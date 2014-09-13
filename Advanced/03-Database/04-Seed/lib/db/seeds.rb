5d953ab3e1ef0ab7a3242c97b20d6aee8b7dca5a
Amir TOLY <amir.toly@gmail.com>
2014-09-14 00:51:58 +0200

    04-Seed

--- Advanced/03-Database/04-Seed/lib/db/seeds.rb
+++ Advanced/03-Database/04-Seed/lib/db/seeds.rb
@@ -10,4 +10,54 @@ puts 'Seeding database...'
 
 # TODO: Your code goes here
 # 1. create fake users
+
+idx = 0
+
+while idx < 3
+  User.create(
+    name:   Faker::Name.name,
+    email:  Faker::Internet.email)
+
+  idx += 1
+end
+
 # 2. Scrape news from https://news.ycombinator.com/ and associate posts to your existing users
+
+doc = Nokogiri::HTML(open('https://news.ycombinator.com/'))#TODO: Set encoding
+
+posts = []
+
+# Search for nodes by xpath
+doc.xpath("//tr").each do |tr|
+  post = {}
+
+  tr.xpath("td[@class='title' and count(*)=2]/a").each do |name|
+    post[:date] = Time.now#TODO: Consider the actual date instead
+
+    post[:name] = name.content
+  end
+
+  tr.xpath("td[@class='subtext']/span").each do |rating|
+    posts.last[:rating] = rating.content.tr(' points', '').to_i#TODO: What if only 1 point?
+  end
+
+  tr.xpath("td[@class='title']/a[not(@rel)]/@href").each do |source_url|
+    post[:source_url] = source_url.content
+
+    posts << post
+  end
+end
+
+users = User.all
+
+posts.each_with_index do |post, index|
+  if index < 10
+    post[:user] = users[0]
+  elsif index < 20
+    post[:user] = users[1]
+  elsif index < 30
+    post[:user] = users[2]
+  end
+
+  Post.create(post)
+end
--- Advanced/03-Database/04-Seed/lib/interface.rb
+++ Advanced/03-Database/04-Seed/lib/interface.rb
@@ -3,3 +3,77 @@ require_relative 'models/post'
 require_relative 'models/user'
 
 # TODO: build your interface with login and CRUD tasks on the posts
+
+def ask(prompt)
+  print "#{prompt} "
+  gets.to_s.chomp
+end
+
+def create_post(user)
+  name = ask('Name:')
+  source_url = ask('Source URL:')
+  rating = ask('Rating:')
+  post_params = { name: name, source_url: source_url, date: Time.now, rating: rating }
+
+  # TODO: use ActiveRecord to add a new post for the user logged in!
+  post_params[:user_id] = user.id
+  Post.create(post_params)
+end
+
+def list_posts(user)
+  # TODO: use ActiveRecord to get all posts of the current user
+  posts = Post.where(user_id: user.id)
+
+  puts '-' * 50
+  puts '%-3s %-100s %-150s %-30s %s' % ['#', 'Name', 'Source URL', 'Created At', 'Rating']
+
+  posts.each do |post|
+    puts '%-3d %-100s %-150s %-30s %d' % [post.id, post.name, post.source_url, post.date, post.rating]
+  end
+
+  puts '-' * 50
+end
+
+def list_users
+  users = User.all
+
+  puts '-' * 50
+  puts '%-3s %-30s %-50s' % ['#', 'Name', 'Email']
+
+  users.each do |user|
+    puts '%-3d %-30s %-50s' % [user.id, user.name, user.email]
+  end
+
+  puts '-' * 50
+end
+
+def delete_posts(user)
+  # TODO: use ActiveRecord to delete all posts of current user
+  posts = Post.where(user_id: user.id)
+  posts.destroy_all
+end
+
+user = nil
+
+while true
+
+  until user
+    puts 'Please login with your <email>'
+    list_users
+
+    user = User.find_by_email(ask('>'))
+  end
+
+  puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
+  puts "1. Create a post"
+  puts "2. Read your posts"
+  puts "3. Delete all posts"
+  puts "4. Exit"
+
+  case ask('>').to_i
+  when 1 then create_post(user)
+  when 2 then list_posts(user)
+  when 3 then delete_posts(user)
+  when 4 then break
+  end
+end
