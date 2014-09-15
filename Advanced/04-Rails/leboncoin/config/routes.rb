db9d9a4ddb90400d7f6f2f7e6aba920a056fa16b
Amir TOLY <amir.toly@gmail.com>
2014-09-15 20:34:10 +0200

    Add default route to tags#index

--- Advanced/04-Rails/leboncoin/config/routes.rb
+++ Advanced/04-Rails/leboncoin/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :tags
 
+  root to: 'tags#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
