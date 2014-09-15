568a2534a2a9c5cab2f145210e6f9bd8f88ea73b
Amir TOLY <amir.toly@gmail.com>
2014-09-15 21:17:51 +0200

    Run rake db:migrate

--- Advanced/04-Rails/leboncoin/db/schema.rb
+++ Advanced/04-Rails/leboncoin/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915170500) do
+ActiveRecord::Schema.define(version: 20140915185324) do
+
+  create_table "ads", force: true do |t|
+    t.string   "title"
+    t.integer  "price"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tags", force: true do |t|
     t.string   "name"
