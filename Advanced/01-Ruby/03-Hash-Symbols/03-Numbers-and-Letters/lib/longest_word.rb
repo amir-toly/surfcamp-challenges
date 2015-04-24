da5edb4dedc5dd929e0f03de635f460b2ff264b5
Amir TOLY <amir.toly@gmail.com>
2015-04-25 01:13:12 +0200

    WIP

--- Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
+++ Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
@@ -8,9 +8,25 @@ end
 
 def run_game(attempt, grid, start_time, end_time)
   #TODO: runs the game and return detailed hash of result
+  translation_json = "N/A"
+
+  open("http://api.wordreference.com/0.8/80143/json/enfr/" + attempt) do |stream|
+    translation_json = JSON.parse(stream.read)
+  end
+
+  if translation_json == "N/A" || translation_json["Error"] == "NoTranslation"
+    translation = "N/A"
+  else
+    translation = translation_json["term0"]["PrincipalTranslations"]["0"]["FirstTranslation"]["term"]
+  end
+
+  attempt_array = attempt.chars
+
+  in_grid = attempt_array.all? { |letter| grid.include?(letter) }
+
   {
     time: (end_time - start_time),
-    translation: "Look at wordreference.com",
+    translation: translation,
     score: "any idea?",
     message: "well done"
   }
